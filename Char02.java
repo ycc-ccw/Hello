79fec73a7796c112ace76e39344f43953c90ce89
ycc-ccw <ycw183@outlook.com>
2024-10-15 19:10:35 +0800

    10.15

---
+++ Char02.java
@@ -0,0 +1,18 @@
+public class Char02 {
+	
+	public static void main(String[] args) {
+		
+		char c1 = 97;
+		System.out.println(c1);
+		
+		char c2 = 'a';
+		System.out.println((int)c2);
+		char c3 = '杨';
+		System.out.println((int)c3);
+		System.out.println('a' + 100);
+		
+		char c4 = 'b' + 1;
+		System.out.println((int)c4);
+		System.out.println(c4);
+	}
+}
