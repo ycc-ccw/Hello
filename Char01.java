ea971d89799c93b4d44101f3b9b1be7e27abb4dc
ycc-ccw <ycw183@outlook.com>
2024-10-15 18:55:15 +0800

    10.15

---
+++ Char01.java
@@ -0,0 +1,15 @@
+public class Char01 {
+	
+	public static void mian(String[] args) {
+		//演示Char的基本使用
+		char c1 = 'a';
+		char c2 = '\t';
+		char c3 = '杨';
+		char c4 = 97;
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		
+	}
+}
