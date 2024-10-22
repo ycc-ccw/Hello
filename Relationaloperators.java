44be6192208c3bd5e00679264ad087d09dd13ced
ycc-ccw <ycw183@outlook.com>
2024-10-22 18:16:54 +0800

    10.22

---
+++ Relationaloperators.java
@@ -0,0 +1,17 @@
+public class Relationaloperators {
+	
+	public static void main(String[] args) {
+		
+		int b = 8;
+		int a = 9;
+		System.out.println(a > b);
+		System.out.println(a >= b);
+		System.out.println(a <= b);
+		System.out.println(a < b);
+		System.out.println(a == b);
+		System.out.println(a !=b );
+		boolean flag = a>b;
+		System.out.println("flag=" + flag);
+		  
+	}	
+}
